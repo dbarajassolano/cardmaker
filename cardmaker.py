7b19ac97958e38a40e15c617f9d753c43f06e47a
David A. Barajas-Solano <dbarajassolano@gmail.com>
2021-07-02 12:29:58 -0700

    Recognition and recall cards for vocabulary notes

--- cardmaker.py
+++ cardmaker.py
@@ -1,4 +1,4 @@
-from cards import CMEntry, CMVocabCard, CMChar, CMKanjiCard
+from CM import VocabEntry, VocabNote, KanjiEntry, KanjiNote
 import genanki
 import random
 import re
@@ -8,17 +8,17 @@ saved_to_collection = False
 exit_flag = False
 
 deck_id      = random.randrange(1 << 30, 1 << 31)
-fields       = [{'name': 'Question'}, {'name': 'Answer'}, {'name': 'Pitch'},]
-fields_np    = [{'name': 'Question'}, {'name': 'Answer'},]
-templates    = [{'name': 'Card 1', 'qfmt': '<p class="question">{{Question}}</p>', 'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}<br><div class=pitchbox>{{Pitch}}</div>',},]
-templates_np = [{'name': 'Card 1', 'qfmt': '<p class="question">{{Question}}</p>', 'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',},]
 style = """
 .card {
  font-size: 20px;
  text-align: center;
 }
-.question {
+.kanji {
  font-size: 40px;
+}
+.pos {
+ font-size: 10px;
+ font-style: italic;
 }
 .pitchbox {
  padding-top: 10px;
@@ -34,17 +34,22 @@ rb, rt {
 """
 
 model_gen    = random.Random(0)
-model_id     = model_gen.randrange(1 << 30, 1 << 31)
-model_np_id  = model_gen.randrange(1 << 30, 1 << 31)
-model        = genanki.Model(model_id, 'CardMaker model', fields=fields, templates=templates, css=style)
-model_np     = genanki.Model(model_np_id, 'CardMaker model', fields=fields_np, templates=templates_np, css=style)
+
+kanji_fields    = [{'name': 'Question'}, {'name': 'Answer'},]
+kanji_templates = [{'name': 'Kanji card', 'qfmt': '<p class="kanji">{{Question}}</p>', 'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',},]
+kanji_model     = genanki.Model(model_gen.randrange(1 << 30, 1 << 31), 'Kanji note model', fields=kanji_fields, templates=kanji_templates, css=style)
+
+vocab_fields    = [{'name': 'KanjiFormFuriganized'}, {'name': 'KanaForm'}, {'name':'Meanings'}, {'name': 'Pitch'}]
+vocab_templates = [{'name': 'Recognition', 'qfmt': '<p class="kanji">{{KanjiFormFuriganized}}</p>', 'afmt': '{{FrontSide}}<hr id="answer">{{KanaForm}}<br>{{Meanings}}<div class=pitchbox>{{Pitch}}</div>',}, {'name': 'Recall', 'qfmt': '<p>{{Meanings}}</p>', 'afmt': '{{FrontSide}}<hr id="answer"><p class="kanji">{{KanjiFormFuriganized}}</p>{{KanaForm}}<div class=pitchbox>{{Pitch}}</div>',}]
+vocab_model     = genanki.Model(model_gen.randrange(1 << 30, 1 << 31), 'Vocab note model', fields=vocab_fields, templates=vocab_templates, css=style)
+
 
 parser = argparse.ArgumentParser(description='Make Anki cards from JMDict/KanjiDic2 searches')
 parser.add_argument('collection', metavar='C', type=str, nargs=1, help='Collection filename')
-parser.add_argument('--deck', dest='deck_name', action='store', type=str, nargs=1, default='CardMaker deck', help='Deck name')
+parser.add_argument('--deck', dest='deck_name', action='store', type=str, nargs='?', default='CardMaker deck', const='CardMaker deck', help='Deck name')
 args = parser.parse_args()
 outfile = args.collection[0]
-deck_name = args.deck_name[0]
+deck_name = args.deck_name
 try:
     if not isinstance(outfile, str) or not outfile:
         print('Invalid output file name')
@@ -53,11 +58,11 @@ except TypeError as exc:
     print('Invalid output file name')
     quit()
 
-print(f'\nWill create deck {deck_name} with id#{deck_id}\n\t...of cards with model id#{model_id}\n\t\t...to file {outfile}\n')
-
 deck = genanki.Deck(deck_id, deck_name)
 
-cards = []
+print(deck_name)
+
+notes = []
 
 edit_pattern   = re.compile(r'^edit [0-9]+$')
 delete_pattern = re.compile(r'^delete [0-9]+$')
@@ -82,26 +87,23 @@ if __name__ == '__main__':
         elif command == 'list':
             idx = 1
             print('')
-            if cards == []:
-                print('No cards yet')
-            for card in cards:
-                print('* Card #{:d}'.format(idx))
-                print(card.short())
+            if notes == []:
+                print('No notes yet')
+            for note in notes:
+                print('* Note #{:d}'.format(idx))
+                print(note.short())
                 idx = idx + 1
             print('')
 
         elif command == 'save':
-            if cards == []:
+            if notes == []:
                 print('Nothing to save yet')
             else:
-                for card in cards:
-                    if not hasattr(card, 'pitch_svg'):
-                        note = genanki.Note(model=model_np, fields=[card.question, card.answer])
+                for note in notes:
+                    if note.is_kanjinote:
+                        note = genanki.Note(model=kanji_model, fields=[note.question, note.answer])
                     else:
-                        if card.pitch_svg:
-                            note = genanki.Note(model=model, fields=[card.question, card.answer, card.pitch_svg])
-                        else:
-                            note = genanki.Note(model=model_np, fields=[card.question, card.answer])
+                        note = genanki.Note(model=vocab_model, fields=[note.kanji_form_furiganized, note.kana_form, note.meanings, note.pitch_svg])
                     deck.add_note(note)
                 genanki.Package(deck).write_to_file(outfile)
                 exit_flag = True
@@ -110,44 +112,44 @@ if __name__ == '__main__':
         elif delete_pattern.search(command):
             idx = int(re.findall("[0-9]+", command)[0]) - 1
             try:
-                del cards[idx]
+                del notes[idx]
             except IndexError:
-                print(f'Invalid card number (Valid: 1-{len(cards)})')
+                print(f'Invalid note number (Valid: 1-{len(notes)})')
                 continue
 
         elif not command:
             continue
 
         else:
-            # Determine if you want to edit a card
+            # Determine if you want to edit a note
             replace_idx = None
             if edit_pattern.search(command):
                 replace_idx = int(re.findall("[0-9]+", command)[0]) - 1
-                if replace_idx >= len(cards):
-                    print(f'Invalid card number (Valid: 1-{len(cards)})')
+                if replace_idx >= len(notes):
+                    print(f'Invalid note number (Valid: 1-{len(notes)})')
                     continue
                 command = input('> New search? ')
                 command = command.encode('utf-8', 'replace').decode()
             if char_pattern.search(command):
                 # Kanji chard
-                char = CMChar(char_pattern.search(command).groups()[0])
+                char = KanjiEntry(char_pattern.search(command).groups()[0])
                 if char.kanjidic_char == None:
                     continue
-                card = CMKanjiCard(char)
+                note = KanjiNote(char)
             else:
-                # Vocabulary card
-                entry = CMEntry(command)
+                # Vocabulary note
+                entry = VocabEntry(command)
                 if entry.jmdict_entry == None:
                     continue
-                card = CMVocabCard(entry)
-            # Replace existing card
+                note = VocabNote(entry)
+            # Replace existing note
             if replace_idx is not None:
-                cards[replace_idx] = card
-                print(f'\n{cards[replace_idx]}\n')
+                notes[replace_idx] = note
+                print(f'\n{notes[replace_idx]}\n')
             else:
-                # Add new card to list of cards
-                cards.append(card)
-                print(f'\n{cards[-1]}\n')
+                # Add new note to list of notes
+                notes.append(note)
+                print(f'\n{notes[-1]}\n')
 
 
 quit()
